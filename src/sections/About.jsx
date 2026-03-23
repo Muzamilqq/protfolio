04141a16d1021109aa063b084e71eda470af4b79
Muzamil <ashrafmuzamil329@gmail.com>
2026-03-23 23:35:59 +0500

    lag fix

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -20,7 +20,7 @@ const About = () => {
           <div className="z-10">
             <p className="headtext">Hi, I'm Muzamil Ashraf</p>
             <p className="subtext">
-              Over the last 1 years, I developed my frontend and backend dev
+              Over the last 1.5 years, I developed my frontend and backend dev
               skills to deliver dynamic and software and web applications.
             </p>
           </div>
